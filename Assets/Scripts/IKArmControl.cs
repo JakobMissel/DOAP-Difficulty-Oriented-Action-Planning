0400fedac9df50bc02070e38fa4844bfddd5b0bd
Akaidus <nikolajjensen6854@gmail.com>
2025-10-08 13:56:19 +0200

    IKArmControl Update
    + Attempt to make transition between left and right hand on wall. Still WIP.

--- Assets/Scripts/IKArmControl.cs
+++ Assets/Scripts/IKArmControl.cs
@@ -4,12 +4,14 @@ using UnityEngine.Animations.Rigging;
 public class IKArmControl : MonoBehaviour
 {
     Rig rig;
+    [SerializeField] float differenceThreshold = 0.25f;
     [Header("Left")]
     [SerializeField] GameObject leftArmConstraints;
     [SerializeField] GameObject leftTarget;
     [SerializeField] Transform leftShoulder;
+    [SerializeField] float leftDistanceToShoulder = 0.5f;
+    
     Vector3 leftTargetStartPosition;
-    float leftDistanceToShoulder = 0.8f;
     TwoBoneIKConstraint leftArmIK;
     MultiRotationConstraint leftHandRotation;
     Vector3 leftClosestPoint;
@@ -18,8 +20,9 @@ public class IKArmControl : MonoBehaviour
     [SerializeField] GameObject rightArmConstraints;
     [SerializeField] GameObject rightTarget;
     [SerializeField] Transform rightShoulder;
+    [SerializeField] float rightDistanceToShoulder = 0.5f;
+    
     Vector3 rightTargetStartPosition;
-    float rightDistanceToShoulder = 0.8f;
     TwoBoneIKConstraint rightArmIK;
     MultiRotationConstraint rightHandRotation;
     Vector3 rightClosestPoint;
@@ -29,6 +32,7 @@ public class IKArmControl : MonoBehaviour
     void Awake()
     {
         rig = GetComponent<Rig>();
+        rig.weight = 1;
 
         leftArmIK = leftArmConstraints.GetComponent<TwoBoneIKConstraint>();
         leftHandRotation = leftArmConstraints.GetComponent<MultiRotationConstraint>();
@@ -73,29 +77,28 @@ public class IKArmControl : MonoBehaviour
         var leftDistance = Vector3.Distance(leftShoulder.position, leftClosestPoint);
         var rightDistance = Vector3.Distance(rightShoulder.position, rightClosestPoint);
 
-        var rightWeight = Mathf.Clamp01(1 - (rightDistance / rightDistanceToShoulder));
-        rig.weight = rightWeight;
-        rightArmIK.weight = rightWeight;
-        rightHandRotation.weight = rightWeight;
-        rightTarget.transform.position = rightClosestPoint;
+        //var leftWeight = Mathf.Clamp01(1 - (leftDistance / leftDistanceToShoulder));
+        //leftArmIK.weight = leftWeight;
+        //leftHandRotation.weight = leftWeight;
+        //leftTarget.transform.position = leftClosestPoint;
 
-        var weight = Mathf.Clamp01(1 - (leftDistance / leftDistanceToShoulder));
-        rig.weight = weight;
-        leftArmIK.weight = weight;
-        leftHandRotation.weight = weight;
-        leftTarget.transform.position = leftClosestPoint;
-        /*
+        //var rightWeight = Mathf.Clamp01(1 - (rightDistance / rightDistanceToShoulder));
+        //rig.weight = rightWeight;
+        //rightArmIK.weight = rightWeight;
+        //rightHandRotation.weight = rightWeight;
+        //rightTarget.transform.position = rightClosestPoint;
+
+        //rig.weight = leftDistance > rightDistance ? leftWeight : rightWeight;
+
         if ((leftDistance < leftDistanceToShoulder) && leftDistance < rightDistance)
         {
             var weight = Mathf.Clamp01(1 - (leftDistance / leftDistanceToShoulder));
-            rig.weight = weight;
             leftArmIK.weight = weight;
             leftHandRotation.weight = weight;
             leftTarget.transform.position = leftClosestPoint;
         }
         else
         {
-            rig.weight = 0;
             leftArmIK.weight = 0;
             leftHandRotation.weight = 0;
             leftTarget.transform.position = leftTargetStartPosition;
@@ -103,19 +106,30 @@ public class IKArmControl : MonoBehaviour
         if ((rightDistance < rightDistanceToShoulder) && rightDistance < leftDistance)
         {
             var weight = Mathf.Clamp01(1 - (rightDistance / rightDistanceToShoulder));
-            rig.weight = weight;
             rightArmIK.weight = weight;
             rightHandRotation.weight = weight;
             rightTarget.transform.position = rightClosestPoint;
         }
         else
         {
-            rig.weight = 0;
             rightArmIK.weight = 0;
             rightHandRotation.weight = 0;
             rightTarget.transform.position = rightTargetStartPosition;
         }
-        */
+        if (Mathf.Abs(rightDistance - leftDistanceToShoulder) < differenceThreshold)
+        {
+            var leftWeight = Mathf.Clamp01(1 - (leftDistance / leftDistanceToShoulder));
+            var rightWeight = Mathf.Clamp01(1 - (rightDistance / rightDistanceToShoulder));
+            var weight = leftDistance > rightDistance ? leftWeight + differenceThreshold : rightWeight + differenceThreshold;
+
+            rightArmIK.weight = weight;
+            rightHandRotation.weight = weight;
+            rightTarget.transform.position = rightClosestPoint;
+
+            leftArmIK.weight = weight;
+            leftHandRotation.weight = weight;
+            leftTarget.transform.position = leftClosestPoint;
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -146,10 +160,8 @@ public class IKArmControl : MonoBehaviour
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(leftTarget.transform.position, 0.1f);
-        Gizmos.DrawWireSphere(rightTarget.transform.position, 0.1f);
-        Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(leftClosestPoint, 0.1f);
+        Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(rightClosestPoint, 0.1f);
     }
 }
