a4e3f7b81d9ec719a70c9d1709add5cd239af495
Soya <Axewu_Reitzel@hotmail.com>
2025-10-17 15:08:46 +0200

    Created the base scripts for the InvestigatePlayerNoise Agent behaviour

---
+++ Assets/Scripts/GOAP/Goals/InvestigatePlayerNoiseGoal.cs
@@ -0,0 +1,9 @@
+using CrashKonijn.Goap.Runtime;
+
+namespace Assets.Scripts.GOAP
+{
+    [GoapId("InvestigatePlayerNoise-7b6245d4-dac0-40df-bcf5-f30cc6ed9297")]
+    public class InvestigatePlayerNoiseGoal : GoalBase
+    {
+    }
+}
