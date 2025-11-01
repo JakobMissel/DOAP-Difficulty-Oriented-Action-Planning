a45573cfa47d9e2e377b359cc9de9a35999e1a46
DeadPieGamer <felix@krog.dk>
2025-11-01 19:30:41 +0100

    Painting stealing time is only calculated from after tutorial is finished

--- Assets/Scripts/DDA/DdaPlayerActions.cs
+++ Assets/Scripts/DDA/DdaPlayerActions.cs
@@ -69,6 +69,7 @@ namespace Assets.Scripts.DDA
         {
             PlayerActions.stealItem += (pickup) => PaintingPickedUp();
             PlayerActions.paintingDelivered += PaintingDelivered;
+            PlayerActions.tutorialCompletion += PaintingDelivered;
             PlayerActions.ammoUpdate += UsedItem;
         }
 
@@ -76,6 +77,7 @@ namespace Assets.Scripts.DDA
         {
             PlayerActions.stealItem -= (pickup) => PaintingPickedUp();
             PlayerActions.paintingDelivered -= PaintingDelivered;
+            PlayerActions.tutorialCompletion -= PaintingDelivered;
             PlayerActions.ammoUpdate -= UsedItem;
         }
 
