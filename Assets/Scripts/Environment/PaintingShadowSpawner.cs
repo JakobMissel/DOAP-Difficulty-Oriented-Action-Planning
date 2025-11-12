56fc8c34b771b236c601dad467f24e2f079493b6
DeadPieGamer <felix@krog.dk>
2025-11-12 11:20:06 +0100

    Painting Shadows

---
+++ Assets/Scripts/Environment/PaintingShadowSpawner.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PaintingShadowSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject shadowPrefab;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Instantiate(shadowPrefab, transform.position, transform.rotation);
+        Destroy(this);
+    }
+}
