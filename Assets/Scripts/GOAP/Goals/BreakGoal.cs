ba54a383d33cf3e53a3f6d58b680dbf5a03dd6c7
Soya <Axewu_Reitzel@hotmail.com>
2025-11-19 13:02:43 +0100

    Removed unused GOAP Actions and Goals
    -InvestigatePlayerNoise (uses BrainBehaviour to hear)
    -Break (only uses recharge)
    -ContactBackup (2 guards in the scene)

--- Assets/Scripts/GOAP/Goals/BreakGoal.cs
+++
@@ -1,9 +0,0 @@
-using CrashKonijn.Goap.Runtime;
-
-namespace Assets.Scripts.GOAP
-{
-    [GoapId("Break-3fbb6fe8-6a30-42bb-a731-3d1003d9a4b5")]
-    public class BreakGoal : GoalBase
-    {
-    }
-}
