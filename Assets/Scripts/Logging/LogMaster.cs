f0ce6eaed0ac34a78eb39b038b56cbce00ab83b8
DeadPieGamer <felix@krog.dk>
2025-11-27 23:43:36 +0100

    Log maybe better

--- Assets/Scripts/Logging/LogMaster.cs
+++ Assets/Scripts/Logging/LogMaster.cs
@@ -121,7 +121,7 @@ namespace Assets.Scripts.Logging
             // Write out the data
             for (int i = 0; i < ddaLogData.Count; i++)
             {
-                writer.WriteLine($"{ddaLogData[i].time:N2};{ddaLogData[i].difficulty:N2}");
+                writer.WriteLine($"{ddaLogData[i].time:0.00};{ddaLogData[i].difficulty}");
             }
 
             // Close the writer
